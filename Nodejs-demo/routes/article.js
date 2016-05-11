7ca0defc078a0a4add3464e1c3759f65c8274034
Laravel-shop <pgy1@foxmail.com>
2016-05-11 14:18:40 +0800

    更新文章代码

--- Nodejs-demo/routes/article.js
+++ Nodejs-demo/routes/article.js
@@ -1,17 +1,8 @@
 var express = require('express');
-//var React = require('react');
 var router = express.Router();
-
-//component组件
-//var ArticleDemo = React.createFactory(require('../components/article'));
 
 ///* GET home page. */
 router.get('/', function(req, res, next) {
-    //var user = userDao.queryById(req, res, next);
-    //JSON.stringify(user);
-    //ArticleDemo.constructor(user);
-    //var demo = React.renderToString(ArticleDemo());
-  //res.render('article', { title: 'user',demo:demo});
   res.render('article', { title: 'Express-测试版'});
 });
 
